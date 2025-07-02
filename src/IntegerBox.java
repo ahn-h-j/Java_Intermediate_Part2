bf848e4808b0941b36be89efb41d18c2eb85dfa5
ahn-h-j <tkgkd246@gmail.com>
2025-07-02 16:58:10 +0900

    Generic 사용 이유 -1

---
+++ src/IntegerBox.java
@@ -0,0 +1,12 @@
+public class IntegerBox {
+
+    private Integer value;
+
+    public void set(Integer value) {
+        this.value = value;
+    }
+
+    public Integer get() {
+        return value;
+    }
+}
