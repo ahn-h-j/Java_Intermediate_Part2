5a4a2f4626ecd98f57ccf87054806c6cacc5b5da
ahn-h-j <tkgkd246@gmail.com>
2025-07-02 17:00:29 +0900

    Generic 사용 이유 -2

---
+++ src/ObjectBox.java
@@ -0,0 +1,12 @@
+public class ObjectBox {
+
+    private Object value;
+
+    public void set(Object object) {
+        this.value = object;
+    }
+
+    public Object get() {
+        return value;
+    }
+}
