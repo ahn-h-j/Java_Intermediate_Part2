2397d54662ef6daa7eb2ff56525fdf80387bb251
ahn-h-j <tkgkd246@gmail.com>
2025-07-02 23:04:55 +0900

    Generic Type Parameter Constraints

---
+++ src/generic/AnimalHospitalMainV1.java
@@ -0,0 +1,22 @@
+package generic;
+
+public class AnimalHospitalMainV1 {
+    public static void main(String[] args) {
+        AnimalHospitalV1<Dog> dogHospital = new AnimalHospitalV1<>();
+        AnimalHospitalV1<Cat> catHospital = new AnimalHospitalV1<>();
+
+        Dog dog = new Dog("멍멍이1", 100);
+        Cat cat = new Cat("냐옹이1", 300);
+
+        dogHospital.set(dog);
+        dogHospital.checkup();
+        Dog biggerDog = dogHospital.getBigger(new Dog("멍멍이2", 200));
+        System.out.println("biggerDog = " + biggerDog);
+
+        catHospital.set(cat);
+        catHospital.checkup();
+
+
+    }
+
+}
---
+++ src/generic/AnimalHospitalV1.java
@@ -0,0 +1,16 @@
+package generic;
+
+public class AnimalHospitalV1<T extends Animal> {
+    private T animal;
+    public void set(T animal) {
+        this.animal = animal;
+    }
+    public void checkup() {
+        System.out.println("동물 이름: " + animal.getName());
+        System.out.println("동물 크기: " + animal.getSize());
+        animal.sound();
+    }
+    public T getBigger(T target) {
+        return animal.getSize() > target.getSize() ? animal : target;
+    }
+}
